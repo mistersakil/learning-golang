31ef1145e21748ddfe78ea2c0f51b562f502c266
Sakil Jomadder <sakil.diu.cse@gmail.com>
2026-02-28 11:49:22 +0600

    practice of how to take go user inputs into console

--- functions.go
+++ functions.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func add(num1 float64, num2 float64) float64 {
 	return num1 + num2
@@ -12,7 +10,19 @@ func getNumbers(num1 int, num2 int) (int, int) {
 	return num1 + num2, num1 * num2
 }
 
+func getInputs() (int, int) {
+	num1 := 0
+	num2 := 0
+	fmt.Println("Enter first number:")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter second number:")
+	fmt.Scanln(&num2)
+	return num1, num2
+}
+
 func main() {
-	fmt.Println(add(5.00000005, 3.00000003))
-	fmt.Println(getNumbers(10, 20))
+	num1, num2 := getInputs()
+	fmt.Println("The sum of numbers is:", add(float64(num1), float64(num2)))
+	// fmt.Println(getNumbers(10, 20))
+	// fmt.Println(add(5.00000005, 3.00000003))
 }
