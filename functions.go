7b239ec59b22c6563f6a99379bb36837bed2da08
Sakil Jomadder <sakil.diu.cse@gmail.com>
2026-02-28 11:16:13 +0600

    go multiple return

--- functions.go
+++ functions.go
@@ -8,6 +8,11 @@ func add(num1 float64, num2 float64) float64 {
 	return num1 + num2
 }
 
+func getNumbers(num1 int, num2 int) (int, int) {
+	return num1 + num2, num1 * num2
+}
+
 func main() {
 	fmt.Println(add(5.00000005, 3.00000003))
+	fmt.Println(getNumbers(10, 20))
 }
