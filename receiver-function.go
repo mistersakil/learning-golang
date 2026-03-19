10475b2f25d5053961a068f87bde071881bc6e5f
Sakil Jomadder <sakil.diu.cse@gmail.com>
2026-03-19 13:54:27 +0600

    example of receiver function

--- receiver-function.go
+++ receiver-function.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name  string
+	email string
+}
+
+func bio(newPerson Person) {
+	fmt.Printf("name is %s\n", newPerson.name)
+	fmt.Printf("email is %s\n", newPerson.email)
+}
+
 func main() {
 	fmt.Println("Learning receiver function")
+	newPerson := Person{
+		name:  "sakil jomadder",
+		email: "sakil.jomadder@example.com",
+	}
+	bio(newPerson)
 }
